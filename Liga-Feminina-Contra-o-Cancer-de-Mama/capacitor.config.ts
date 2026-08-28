b6b4a1250e9d15b8f8f8e1698ec6681a9476a81c
João_Vuelma <joaovuelma23@gmail.com>
2026-08-28 19:38:32 -0300

    atualizações

---
+++ Liga-Feminina-Contra-o-Cancer-de-Mama/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Liga-Feminina-Contra-o-Cancer-de-Mama',
+  webDir: 'www'
+};
+
+export default config;
---
+++ Liga-Feminina-Contra-o-Cancer-de-Mama/src/environments/environment.example.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://xxxxx.supabase.co',
+  supabaseKey: 'sua-anon-key-aqui'
+};
